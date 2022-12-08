38de421750dd6994eb93430934caf028340728e5
Jamie Temple <jamie-temple@live.de>
2022-12-08 10:58:01 +0100

    feat: extracted control points

--- src/controllers/bezierCurveController.ts
+++ src/controllers/bezierCurveController.ts
@@ -1,24 +1,79 @@
-import { Vector3 } from "three";
+import { Group, Vector3 } from "three";
 import { Canvas } from "../core/canvas";
-import { primaryColor, primaryColorMax } from "../core/color";
+import { primaryColor } from "../core/color";
 import { CustomPoint, Shape } from "../core/customPoint";
 import { Controller } from "./controller";
 import { BezierCurve } from "../components/bezierCurve";
 import { BezierCurveHelper } from "../components/bezierCurveHelper";
-import { connect } from "../core/connector";
+import { connect, Signal, Slot } from "../core/connector";
 import { PolynomialBasis } from "../components/polynomialBasis";
+import { randFloat } from "three/src/math/MathUtils";
 
+class ControlPoints extends Group {
+
+    public signalChanged: Signal<null>;
+    public _max: number;
+    public _points: Array<CustomPoint>;
+
+    constructor() {
+        super();
+        this.signalChanged = new Signal<null>();
+        this._points = new Array<CustomPoint>();
+        for (let idx = 0; idx < primaryColor.length; idx++) {
+            const point = this.addPoint(new Vector3(
+                randFloat(-10, 10),
+                randFloat(-10, 10),
+                0
+            ));
+            point.name = `${idx}`;
+            this._points.push(point);
+        }
+
+        this._max = 4;
+        this.max = this._max;
+    }
+
+    public get max(): number {
+        return this._max;
+    }
+
+    public set max(value: number) {
+        this._max = value;
+
+        while (this.children.length > 0) {
+            const child = this.children.pop();
+            child?.removeFromParent();
+        }
+        for (let idx = 0; idx < this._max; idx++) {
+            this.add(this._points[idx]);
+        }
+        this.signalChanged.emit(null);
+    }
+
+    /**
+     * `addPoint` allows add points to the array.
+     */
+    private addPoint(position: Vector3): CustomPoint {
+        let point = new CustomPoint(Shape.CUBE, 1);
+        point.setPosition(position);
+
+        point.dragUpdate = () => this.signalChanged.emit(null);
+        point.color = primaryColor[this._points.length];
+
+        return point;
+    }
+
+
+}
 
 export class BezierCurveController extends Controller {
 
-    step!: number;
-    tangentSize: number;
-
     bezierCurve: BezierCurve;
     bezierCurveHelper: BezierCurveHelper;
-    bezierCurvePoints: Array<CustomPoint>;
-
     polynomialBasis: PolynomialBasis;
+
+    controlPoints: ControlPoints;
+    slotChanged: Slot<null>;
 
     constructor(canvasWidth: () => number, canvasHeight: () => number) {
         super();
@@ -27,9 +82,14 @@ export class BezierCurveController extends Controller {
         this.canvas.push(new Canvas(canvasWidth, canvasHeight));
         this.canvas.push(new Canvas(canvasWidth, canvasHeight, false));
 
-        // 2. set basic variables
-        this.step = .5;
-        this.tangentSize = 5;
+        // 2. control points
+        this.controlPoints = new ControlPoints();
+        this.slotChanged = new Slot<null>();
+        this.slotChanged.addCallable((_) => this.changed());
+        this.canvas[0].append(this.controlPoints);
+        this.controlPoints.children.forEach(c => this.canvas[0].draggable(c));
+
+        connect(this.controlPoints.signalChanged, this.slotChanged);
 
         // 3. create curve
         this.bezierCurve = new BezierCurve();
@@ -42,31 +102,10 @@ export class BezierCurveController extends Controller {
         this.canvas[1].append(this.polynomialBasis);
 
         connect(this.bezierCurve.signalControlPoints, this.bezierCurveHelper.slotControlPoints);
-        connect(this.bezierCurve.signalControlPolygon, this.bezierCurveHelper.slotControlPolygon);        
+        connect(this.bezierCurve.signalControlPolygon, this.bezierCurveHelper.slotControlPolygon);
         connect(this.bezierCurve.signalResolution, this.polynomialBasis.slotResolution);
         connect(this.bezierCurve.signalControlPointsCount, this.polynomialBasis.slotControlPointsCount);
         connect(this.bezierCurveHelper.signalTimeValue, this.polynomialBasis.slotTimeValue);
-
-        // 4. create control points
-        this.bezierCurvePoints = new Array<CustomPoint>();
-        const positions = [
-            new Vector3(-10, -10, 0),
-            new Vector3(0, 10, 0),
-            new Vector3(10, -30, 0),
-            new Vector3(-12, -15, 0),
-        ];
-
-        for (let idx = 0; idx < 4; idx++) {
-            let point = new CustomPoint(Shape.CUBE, 1);
-            point.setPosition(positions[idx]);
-
-            point.dragUpdate = () => this.changed();
-            point.color = primaryColor[idx];
-
-            this.bezierCurvePoints.push(point);
-            this.canvas[0].append(point);
-            this.canvas[0].draggable(point);
-        }
 
         this.changed();
     }
@@ -77,7 +116,12 @@ export class BezierCurveController extends Controller {
      */
     override update(): void {
         if (this.needsUpdate) {
-            let cp = this.bezierCurvePoints.map(p => p.position.clone());
+            let cp = this.controlPoints.children.map((p, idx) => {
+                if (idx < this.controlPoints.max)
+                    return p.position.clone();
+                else
+                    return new Vector3(Number.MAX_SAFE_INTEGER);
+            }).filter(value => value.x !== Number.MAX_SAFE_INTEGER);
             this.bezierCurve.controlPolygon = cp;
             this.needsUpdate = false;
         }
@@ -85,56 +129,27 @@ export class BezierCurveController extends Controller {
 
     override gui(gui: dat.GUI): void {
         const bezierCurve = gui.addFolder("Bezier Curve")
+        const bezierCurveHelper = gui.addFolder("Bezier Curve Helper");
+        const controlPoints = gui.addFolder("Control Points");
+
         bezierCurve.open();
+        bezierCurveHelper.open();
+        controlPoints.open();
+
         bezierCurve.add(this.bezierCurve, "resolution", 16, 256, 2)
             .name("Resolution").onChange(() => this.changed());
         bezierCurve.add(this.bezierCurve, "toggleControlPolygon")
             .name("Toggle Control Polygon");
 
-        const bezierCurveHelper = gui.addFolder("Bezier Curve Helper");
-        bezierCurveHelper.open();
         bezierCurveHelper.add(this.bezierCurveHelper, "t", 0, 1, .01)
-            .name("t (step)").onChange(v=>this.step = v)
+            .name("t (step)");
         bezierCurveHelper.add(this.bezierCurveHelper, "tangentMagnitude", 1, 20, 1)
             .name("Tangent Magnitude");
         bezierCurveHelper.add(this.bezierCurveHelper, "toggleIntermediates")
             .name("Toggle Intermediates");
         bezierCurveHelper.add(this.bezierCurveHelper, "toggleCurrentPoint")
             .name("Toggle Current Point");
-    }
 
-
-    /**
-     * `addPoint` is an UI only function. It allows the user to
-     * extend the list of points.
-     */
-    private addPoint(): void {
-        if (this.bezierCurvePoints.length == primaryColorMax) {
-            return;
-        }
-
-        let point = new CustomPoint(Shape.CUBE, 1);
-        point.setPosition(new Vector3());
-
-        point.dragUpdate = () => this.needsUpdate = true;
-        point.color = primaryColor[this.bezierCurvePoints.length];
-        point.name = `curveControlPoint${this.bezierCurvePoints.length}`;
-
-        this.bezierCurvePoints.push(point);
-        this.canvas[0].draggable(point);
-
-        this.needsUpdate = true;
-    }
-
-    /**
-     * `removePoint` is an UI function. It allows users to
-     * pop the lsit of points.
-     */
-    private removePoint(): void {
-        if (this.bezierCurvePoints.length > 2) {
-            const point = this.bezierCurvePoints.pop();
-            point?.removeFromParent();
-            this.needsUpdate = true;
-        }
+        controlPoints.add(this.controlPoints, "max", 2, this.controlPoints._points.length, 1);
     }
 }  
