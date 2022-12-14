f96eb696f74ce26c3c8964f10e523efd7ec06d0b
Nick Grunert <ngredblack@gmail.com>
2022-12-14 18:54:59 +0100

    refactor: commetn controller base class

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,9 +1,15 @@
 import { Canvas } from "../core/canvas";
 
+/**
+ * abstract super class for controller of specific curve or surface types
+ */
 export abstract class Controller {
     canvas: Array<Canvas> = new Array<Canvas>();
     needsUpdate: boolean = true;
 
+    /**
+     * loop for eternity: handle drawing elements on the canvas and update if needed
+     */
     public run(): void {
         this.canvas.forEach(c => c.draw());
         this.update();
@@ -13,16 +19,22 @@ export abstract class Controller {
         });
     }
 
+    /**
+     * mark the "update needed" boolean so that run knows a redraw is needed
+     */
     public changed(): void {
         this.needsUpdate = true;
     }
+
+    /// -----------------------------------------------------------------------
+    /// ABSTRACT FUNCTIONS FOR OVERRIDE
+    /// -----------------------------------------------------------------------
 
     /**
      * Called in every `requestFrameAnimation`. It performs all updates on the
      * objects.
      */
     abstract update(): void;
-
 
     /**
      * Adds controls to the ui.
