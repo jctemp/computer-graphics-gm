d9d378b5be4fb8297aa25a4c22e80426f67420ce
Jamie Temple <jamie-temple@live.de>
2022-12-16 01:48:13 +0100

    change: more colours

--- src/core/color.ts
+++ src/core/color.ts
@@ -1,23 +1,17 @@
-export const primaryColorMax: number = 8;
 export const primaryColor: Array<number> = [
-    0xFF0000,   // Red
-    0x00FF00,   // Green
-    0x0000FF,   // Blue
-
-    0xFFFF00,   // Yellow
-    0xFF00FF,   // Pink
-    0x00FFFF,   // Cyan
-
-    0xF0F000,
-    0xF000F0,
-    0x00F0F0,
-    
-    0x0F0F00,   
-    0x0F000F,  
-    0x000F0F,  
-];
-export const secondaryColorMax: number = 3;
-export const secondaryColor: Array<number> = [
-    0xFF7F00,   // Orange
-    0x7F00FF,   // Violet
+    0x14e6ad,
+    0xe0ef22,
+    0x8d5aa6,
+    0x29b4fa,
+    0xadbe7f,
+    0x287d68,
+    0xd7d17c,
+    0xc528b7,
+    0x937350,
+    0x61b6c5,
+    0x70f696,
+    0x21cd35,
+    0xfc6b03,
+    0x4c824f,
+    0xa5322a,
 ];
