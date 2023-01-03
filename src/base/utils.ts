c1628881abae8149f6b90d92f6ffd8d7611d96a1
Nick Grunert <ngredblack@gmail.com>
2023-01-03 20:44:20 +0100

    feat: comment utils

--- src/base/utils.ts
+++ src/base/utils.ts
@@ -1,32 +1,34 @@
 import { Vector3 } from "three";
 
+/**
+ * calculate lerp between a and b with factor t.
+ */
 function lerp(a: Vector3, b: Vector3, t: number): Vector3 {
     return b.clone().sub(a).multiplyScalar(t).add(a);
 }
 
+/**
+ * round value with n decimal positions (default 5 digits).
+ */
 function roundN(value: number, n: number = 5) {
     const k = 10 ** n;
     return Math.round(value * k) / k;
 }
 
 /**
- * Calculates the binomial coefficients.
+ * calculate the binomial coefficient n over k.
  */
 function binomial(n: number, k: number): number {
     let coefficient = 1;
-    for (let x = n - k + 1; x <= n; x++) {
-        coefficient *= x;
-    }
 
-    for (let x = 1; x <= k; x++) {
-        coefficient /= x;
-    }
+    for (let x = n - k + 1; x <= n; x++) coefficient *= x;
+    for (let x = 1; x <= k; x++) coefficient /= x;
 
     return coefficient;
 }
 
 /**
- * transpose a given 2d number vector
+ * transpose a given 2d array of any kind.
  */
 function transpose(matrix: any[][]): any[][] {
     if (matrix.length === 0) return []
